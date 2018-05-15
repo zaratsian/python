16306c22e3ef9018367406ede58dc3d881ec79aa
Dan Zaratsian <dtz001@gmail.com>
2018-05-15 19:43:55 -0400

    Update twitter_search.py

--- twitter_search.py
+++ twitter_search.py
@@ -2,7 +2,7 @@
 ##################################################################################################################################
 #
 #   Twitter - Collect tweets based on search term
-#   Created in 2016
+#   Created in 2016, recently tested on 15May2018 with python Anaconda 3.6.4
 #
 #   References:
 #       https://developer.twitter.com/en/docs/api-reference-index
