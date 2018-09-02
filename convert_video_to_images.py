bd6227ab158480ebf5c5a011e08268194da62f1d
Dan Zaratsian <dtz001@gmail.com>
2018-09-02 10:31:24 -0400

    Update convert_video_to_images.py

--- convert_video_to_images.py
+++ convert_video_to_images.py
@@ -4,51 +4,65 @@
 #   Convert Video (.MOV) to several image files (.jpeg)
 #
 #   Usage:
-#       file.py <video_file> <number_of_images_per_second>
-#       file.py "/assets/IMG_7942.MOV" 2
+#       file.py --image_prefix IMAGE_PREFIX --video_filepath VIDEO_FILEPATH --sampling_rate SAMPLING_RATE [--image_width IMAGE_WIDTH]
+#       file.py --image_prefix oceans_11 --video_filepath /tmp/youtube_imm6OR605UI.mp4 --sampling_rate 1000 --image_width 400
 #
 #   Dependencies:
 #       sudo apt-get install python-opencv
 #
+#   To Do:
+#       Add exception handling
+#
 ####################################################################################################
 
-import sys
+import sys,re
 import datetime,time
 import cv2
+import argparse
 
 
-try:
-    video_file = sys.argv[1]
-except:
-    print('[ ERROR ] Usage: file.py <video_file> <number_of_images_per_second>')
-    sys.exit()
 
-try:
-    img_per_sec = float(sys.argv[2])
-except:
-    print('[ WARNING ] Did not receive a valid argument. Proceeding with a sampling rate of 1 image per second.')
-    print('[ WARNING ] Usage: file.py <video_file> <number_of_images_per_second>')
-
-
-# Set Desired Image Width and Height
-img_width = 400 # 400 pixels
-
-
-video_obj = cv2.VideoCapture(video_file)
-success,image = video_obj.read()
-count = 1
-while success:
-    video_obj.set(cv2.CAP_PROP_POS_MSEC,(count * (1/float(img_per_sec)) * 1000))    # CAP_PROP_POS_MSEC is the current position of the video file in milliseconds or video capture timestamp.
+def convert_video_to_images(image_prefix, video_filepath, sampling_rate, image_width):
     
-    (h, w) = image.shape[:2]                                                        # Get image height and width
-    r = img_width / float(w)
-    resized_image = cv2.resize(image, (img_width, int(h * r)))                      # Resize image based on img_width, and keep scale / aspect ratio.
+    print('[ INFO ] Converting {} into images within current directory...'.format(video_filepath))
+    time.sleep(3)
+    start_time = datetime.datetime.now()
     
-    cv2.imwrite("frame" + str(count) + ".jpg", resized_image)                       # Save frame as .jpg file      
+    video_obj = cv2.VideoCapture(video_filepath)
     success,image = video_obj.read()
-    print('Processed ' + str(count) + ' frames. Reading new frame...')
-    count += 1
+    frame_count = 1
+    while success:
+        
+        video_obj.set(cv2.CAP_PROP_POS_MSEC,(frame_count * sampling_rate))      # CAP_PROP_POS_MSEC is the current position of the video file in milliseconds or video capture timestamp.
+        
+        (h, w) = image.shape[:2]                                                # Get image height and width
+        r = image_width / float(w)
+        resized_image = cv2.resize(image, (image_width, int(h * r)))            # Resize image based on image_width, and keep scale / aspect ratio.
+        
+        frame_timestamp = re.sub('(\:|\.)','_',str(datetime.timedelta(seconds=frame_count * (sampling_rate/1000))).zfill(15))
+        cv2.imwrite("{}_{}.jpg".format(image_prefix, frame_timestamp), resized_image)               # Save frame as .jpg file
+        success,image = video_obj.read()
+        frame_count += 1
+        if (frame_count % 500) == 0:
+            print('[ INFO ] {} frames have been processed.'.format(frame_count))
+    
+    end_time = datetime.datetime.now()
+    run_time = (end_time - start_time).seconds
+    print('[ INFO ] Complete - Processed {} frames in {} seconds'.format(frame_count, run_time) )
 
+
+
+if __name__ == "__main__":
+    
+    ap = argparse.ArgumentParser()
+    ap.add_argument("--image_prefix",   required=True,            help="Prefix / Name that is appended to each image filename")
+    ap.add_argument("--video_filepath", required=True,            help="Video file path")
+    ap.add_argument("--sampling_rate",  required=True,  type=int, help="Sampling Rate (in milliseconds). i.e. 500 ms sampling will produce 2 images per second")
+    ap.add_argument("--image_width",    required=False, type=int, default=400, help="Desired image width, in pixels (Default: 400 pixels)")
+    args = vars(ap.parse_args())
+    
+    convert_video_to_images(args['image_prefix'], args['video_filepath'], args['sampling_rate'], args['image_width'])
+    
 
 
 #ZEND
